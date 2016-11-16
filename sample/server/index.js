3f9dd33a9f7cf2d531d43a9030696a23fc4ba5ed
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-11-16 01:45:49 -0200

    chore(sample): adding sample folder

---
+++ sample/server/index.js
@@ -0,0 +1,19 @@
+const Ambox = require('../..');
+
+const app = new Ambox();
+const { PORT = 3000, HOST = '0.0.0.0' } = process.env;
+
+app.use(function *() {
+  this.body = 'Hello!';
+});
+
+const server = app.listen(PORT, HOST, error => {
+  error && console.error(error);
+  console.log(`Listening http://${HOST}:${PORT}/`);
+}).on('request', function(request, response) {
+  console.log(`Requesting:`, request, response);
+})
+
+console.log('toJson:', Ambox.toJSON());
+
+module.exports = server;
